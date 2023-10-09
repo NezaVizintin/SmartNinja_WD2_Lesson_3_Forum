442f748330ece54b3ec5b5f4afc13cd6debb3055
Neža Vižintin <neza.vizintin@yahoo.com>
2023-10-09 19:31:56 +0200

    Fixed error

--- models/topic.py
+++ models/topic.py
@@ -10,7 +10,7 @@ class Topic(db.Model):
     topic_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     text = db.Column(db.String)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    author_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
     author = db.relationship(User)
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
