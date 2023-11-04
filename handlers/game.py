fdb6e99417f44d115166040a25ecd4efef3b219b
Neža Vižintin <neza.vizintin@yahoo.com>
2023-11-04 18:08:56 +0100

    Added game and started on website design improvements.

---
+++ handlers/game.py
@@ -0,0 +1,8 @@
+from flask import render_template, request, Blueprint
+
+game_handlers = Blueprint("game_handlers", __name__)
+
+@game_handlers.route("/game", methods=["GET"])
+def game():
+    if request.method == "GET":
+        return render_template("game.html")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from handlers.user import user_get
 from models.settings import db
-from handlers import authorisation, landing, topic, user, comment
+from handlers import authorisation, landing, topic, user, comment, game
 
 app = Flask(__name__)
 
 db.create_all()
 
+# makes function available to all blueprints
 @app.context_processor
 def context_processor():
     user = user_get()
@@ -17,6 +18,7 @@ app.register_blueprint(landing.landing_handlers)
 app.register_blueprint(topic.topic_handlers)
 app.register_blueprint(user.user_handlers)
 app.register_blueprint(comment.comment_handlers)
+app.register_blueprint(game.game_handlers)
 
 if __name__ == "__main__":
     app.run()
